1202212d34ddb28e64b554e0cdbf5983673cbceb
huhan <hu_han_2007@hotmail.com>
2018-08-20 17:29:01 +0800

    ocelot example

---
+++ OcalotProductService/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OcalotProductService
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        public IConfiguration Configuration { get; }
+
+        public ProductController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return HttpContext.Request.Host.Port + " " + Configuration["AppName"] + " " + DateTime.Now.ToString();
+        }
+
+        [HttpGet("/health")]
+        public IActionResult Heathle()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ OcelotServiceManager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot;
+using Ocelot.Middleware;
+
+namespace OcelotServiceManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+     WebHost.CreateDefaultBuilder(args)
+         .ConfigureAppConfiguration((context, builder) => {
+             builder.SetBasePath(context.HostingEnvironment.ContentRootPath)
+             .AddJsonFile("Ocelot.json");
+         })
+         .UseUrls("http://localhost:6000")
+         .UseStartup<Startup>()
+         .Build();
+    }
+}
